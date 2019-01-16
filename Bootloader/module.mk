2c8ad258fe759d2b8bdb13b67d7851fd024b5772
Robin Quint <rob2309@hotmail.de>
2019-01-16 11:43:38 +0100

    Improved module structure

--- Bootloader/module.mk
+++ Bootloader/module.mk
@@ -2,28 +2,27 @@ localdir := Bootloader
 
 include_paths := dep/gnu-efi/inc dep/gnu-efi/inc/x86_64
 
-C_HEADERS := $(wildcard $(localdir)/*.h)
-C_SOURCES := $(wildcard $(localdir)/*.c)
-CPP_SOURCES := $(wildcard $(localdir)/*.cpp)
-C_OBJECTS := ${C_SOURCES:.c=.o} ${CPP_SOURCES:.cpp=.o}
+C_HEADERS := $(wildcard $(localdir)/src/*.h)
+C_SOURCES := $(wildcard $(localdir)/src/*.c)
+
+C_OBJECTS := $(subst /src/,/obj/, $(subst .c,.o, $(C_SOURCES)))
 
 bootloader_filename := BOOTX64.EFI
 
-local_partition_files := $(addprefix $(localdir)/, $(bootloader_filename) msg.txt)
+local_partition_files := $(addprefix $(localdir)/, bin/$(bootloader_filename) res/msg.txt)
 
 root_partition_img_deps += ${local_partition_files}
 
-clean_files += $(addprefix $(localdir)/, *.o *.EFI)
+clean_files += $(addprefix $(localdir)/, obj/*.o bin/*.EFI)
 
 
 
-$(localdir)/$(bootloader_filename): ${C_OBJECTS}
+$(localdir)/bin/$(bootloader_filename): ${C_OBJECTS}
+	@ mkdir -p $(localdir)/bin
 	x86_64-w64-mingw32-gcc -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o $@ $^ -lgcc
 
 
 
-$(localdir)/%.o: $(localdir)/%.c ${C_HEADERS}
-	x86_64-w64-mingw32-gcc -ffreestanding $(addprefix -I, ${include_paths}) -c $< -o $@
-
-$(localdir)/%.o: $(localdir)/%.cpp ${C_HEADERS}
-	x86_64-w64-mingw32-gcc -ffreestanding $(addprefix -I, ${include_paths}) -c $< -o $@
+$(localdir)/obj/%.o: $(localdir)/src/%.c $(C_HEADERS)
+	@ mkdir -p $(localdir)/obj
+	x86_64-w64-mingw32-gcc -ffreestanding $(addprefix -I, $(include_paths)) -c $< -o $@
