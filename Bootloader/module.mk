902a5125def0568180e85adc112bfa479c6fab30
Robin Quint <rob2309@hotmail.de>
2019-01-16 08:06:39 +0100

    Added basic cpp support

--- Bootloader/module.mk
+++ Bootloader/module.mk
@@ -4,7 +4,8 @@ include_paths := dep/gnu-efi/inc dep/gnu-efi/inc/x86_64
 
 C_HEADERS := $(wildcard $(localdir)/*.h)
 C_SOURCES := $(wildcard $(localdir)/*.c)
-C_OBJECTS := ${C_SOURCES:.c=.o}
+CPP_SOURCES := $(wildcard $(localdir)/*.cpp)
+C_OBJECTS := ${C_SOURCES:.c=.o} ${CPP_SOURCES:.cpp=.o}
 
 bootloader_filename := BOOTX64.EFI
 
@@ -18,4 +19,7 @@ $(localdir)/$(bootloader_filename): ${C_OBJECTS}
 	x86_64-w64-mingw32-gcc -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o $@ $^ -lgcc
 
 $(localdir)/%.o: $(localdir)/%.c ${C_HEADERS}
+	x86_64-w64-mingw32-gcc -ffreestanding $(addprefix -I, ${include_paths}) -c $< -o $@
+
+$(localdir)/%.o: $(localdir)/%.cpp ${C_HEADERS}
 	x86_64-w64-mingw32-gcc -ffreestanding $(addprefix -I, ${include_paths}) -c $< -o $@
