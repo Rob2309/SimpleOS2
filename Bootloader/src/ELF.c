ea8fe3d44aea1295f9864f032ad26ae26b0033f0
Robin Quint <rob2309@hotmail.de>
2019-01-25 16:36:18 +0100

    Added Rela relocation handling

--- Bootloader/src/ELF.c
+++ Bootloader/src/ELF.c
@@ -59,10 +59,16 @@ bool PrepareELF(const char* baseImg, char* loadBuffer, Elf64Addr* entryPoint)
         {
             Elf64Addr strtabAddr;
             Elf64Addr symtabAddr;
-            Elf64Addr relAddr;
-            Elf64XWord relSize;
-            Elf64XWord relEntrySize = 24;
-            Elf64XWord relCount;
+
+            Elf64Addr relaAddr = 0;
+            Elf64XWord relaSize = 0;
+            Elf64XWord relaEntrySize = 24;
+            Elf64XWord relaCount = 0;
+
+            Elf64Addr pltrelAddr = 0;
+            Elf64XWord pltrelSize = 0;
+            Elf64XWord pltrelEntrySize = 24;
+            Elf64XWord pltrelCount = 0;
 
             ElfDynamicEntry* dyn = (ElfDynamicEntry*)(loadBuffer + seg->virtualAddress);
             while(dyn->tag != DT_NULL)
@@ -72,23 +78,82 @@ bool PrepareELF(const char* baseImg, char* loadBuffer, Elf64Addr* entryPoint)
                 else if(dyn->tag == DT_SYMTAB)
                     symtabAddr = dyn->value;
                 else if(dyn->tag == DT_JMPREL)
-                    relAddr = dyn->value;
+                    pltrelAddr = dyn->value;
                 else if(dyn->tag == DT_PLTRELSZ)
-                    relSize = dyn->value;
+                    pltrelSize = dyn->value;
                 else if(dyn->tag == DT_RELAENT)
-                    relEntrySize = dyn->value;
+                    pltrelEntrySize = dyn->value;
+                else if(dyn->tag == DT_RELA)
+                    relaAddr = dyn->value;
+                else if(dyn->tag = DT_RELASZ)
+                    relaSize = dyn->value;
+                else if(dyn->tag = DT_RELAENT)
+                    relaEntrySize = dyn->value;
 
                 dyn++;
             }
 
-            relCount = relSize / relEntrySize;
+            relaCount = relaSize / relaEntrySize;
+            pltrelCount = pltrelSize / pltrelEntrySize;
 
-            ElfRelA* relList = (ElfRelA*)(loadBuffer + relAddr);
+            ElfRelA* relaList = (ElfRelA*)(loadBuffer + relaAddr);
+            ElfRelA* pltrelList = (ElfRelA*)(loadBuffer + pltrelAddr);
+
             ElfSymbol* symList = (ElfSymbol*)(loadBuffer + symtabAddr);
             const char* strList = (const char*)(loadBuffer + strtabAddr);
 
-            for(unsigned int i = 0; i < relCount; i++) {
-                ElfRelA* rel = &relList[i];
+            for(unsigned int i = 0; i < relaCount; i++) {
+                ElfRelA* rel = &relaList[i];
+
+                unsigned int symIndex = rel->info >> 32;
+                unsigned int relType = rel->info & 0xFFFFFFFF;
+                Elf64Addr target = (Elf64Addr)loadBuffer + rel->address;
+                Elf64XWord addend = rel->addend;
+
+                /*
+                    B: Image load address
+                    G: offset into GOT
+                    GOT: GOT address
+                    L: PLT address
+                    P: address of reloc target
+                    S: Symbol address
+                    Z: Symbol size
+                    A: Addend
+                */
+
+                printf("Symbol: %s\n", &strList[symList[symIndex].symbolNameOffset]);
+
+                Elf64Addr symAddr = (Elf64Addr)loadBuffer + symList[symIndex].value;
+                Elf64XWord finalAddend = 0;
+                int size = 0;
+
+                switch(relType) {
+                case R_NONE: 
+                case R_COPY: continue;
+                case R_64:      size = 8; finalAddend = symAddr + addend; break;
+                case R_PC32:    size = 4; finalAddend = symAddr + addend - target; break;
+                case R_GLOB_DAT:
+                case R_JUMP_SLOT: size = 8; finalAddend = symAddr; break;
+                case R_RELATIVE: size = 8; finalAddend = addend + (Elf64XWord)loadBuffer; break;
+                case R_32S:
+                case R_32: size = 4; finalAddend = symAddr + addend; break;
+                case R_PC64: size = 8; finalAddend = symAddr + addend - target; break;
+                case R_SIZE32: size = 4; finalAddend = symList[symIndex].size + addend; break;
+                case R_SIZE64: size = 8; finalAddend = symList[symIndex].size + addend; break;
+                default: return 0;
+                }
+
+                switch(size) {
+                case 1: *(unsigned char*)target = finalAddend; break;
+                case 2: *(unsigned short*)target = finalAddend; break;
+                case 4: *(unsigned int*)target = finalAddend; break;
+                case 8: *(unsigned long long*)target = finalAddend; break;
+                default: break;
+                }
+            }
+
+            for(unsigned int i = 0; i < pltrelCount; i++) {
+                ElfRelA* rel = &pltrelList[i];
 
                 unsigned int symIndex = rel->info >> 32;
                 unsigned int relType = rel->info & 0xFFFFFFFF;
--- Kernel/src/main.cpp
+++ Kernel/src/main.cpp
@@ -1,6 +1,9 @@
 
+int g_ReturnValue = 40;
+
 int Test() {
-    return 42;
+    g_ReturnValue += 2;
+    return g_ReturnValue;
 }
 
 extern "C" int main(int argc, char** argv) {
