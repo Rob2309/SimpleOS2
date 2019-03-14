3a4f33f9bf402e7e53c5a958c4f2580bc1f4f2c7
Robin Quint <rob2309@hotmail.de>
2019-03-14 14:45:37 +0100

    Updated depchecker script

--- depcheck.sh
+++ depcheck.sh
@@ -4,20 +4,30 @@ red="\e[31m"
 green="\e[32m"
 reset="\e[0m"
 
+code=0
+
 function check_cmd {
-    printf "Checking wether $2 is installed... "
+    printf "Checking wether $1 is installed... "
     if type $1 1>/dev/zero 2>/dev/zero; then
         echo -e "${green}yes${reset}"
     else
         echo -e "${red}no${reset}"
-        echo -e "${red}SimpleOS2 requires $2 to be installed${reset}"
-        exit 1
+        echo -e "${red}$2${reset}"
+        code=1
     fi
 }
 
-check_cmd ${PE_GCC} "mingw"
-check_cmd ${ELF_GCC} "gcc"
-check_cmd ${NASM} "nasm"
-check_cmd mcopy "mtools"
+check_cmd ${PE_GCC} "SimpleOS2 requires any 64-Bit mingw gcc to be installed. Set the PE_GCC variable accordingly in the Makefile"
+check_cmd ${ELF_GCC} "SimpleOS2 requires any 64-Bit gcc to be installed. Set the ELF_GCC variable accordingly in the Makefile"
+check_cmd ${NASM} "SimpleOS2 requires nasm to be installed"
 
-exit
+check_cmd mmd "SimpleOS2 requires mtools to be installed"
+check_cmd mcopy "SimpleOS2 requires mtools to be installed"
+
+if test $code; then
+    echo -e "${red}Not all dependencies are fullfilled${reset}"
+else
+    echo -e "${green}All dependencies required by SimpleOS2 are installed${reset}"
+fi
+
+exit $code
