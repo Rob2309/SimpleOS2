111ce9be8973bd3430fc9ec53bbe513e63236fb4
Robin Quint <rob2309@hotmail.de>
2019-03-17 14:56:04 +0100

    Improved intermediate names

--- makeinclude/efimodule.mk
+++ makeinclude/efimodule.mk
@@ -7,8 +7,8 @@ c_headers := $(wildcard $(src_dir)/*.h) $(wildcard src/common/*.h)
 c_sources := $(wildcard $(src_dir)/*.c)
 cpp_sources := $(wildcard $(src_dir)/*.cpp)
 
-c_objects := $(subst src/,int/$(config)/, $(subst .c,.o, $(c_sources)))
-cpp_objects := $(subst src/,int/$(config)/, $(subst .cpp,.o, $(cpp_sources)))
+c_objects := $(subst src/,int/$(config)/, $(subst .c,.c.o, $(c_sources)))
+cpp_objects := $(subst src/,int/$(config)/, $(subst .cpp,.cpp.o, $(cpp_sources)))
 
 
 $(output_file): $(c_objects) $(cpp_objects)
@@ -16,9 +16,9 @@ $(output_file): $(c_objects) $(cpp_objects)
 	$(PE_GCC) -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o $@ $^ -lgcc
 
 
-$(int_dir)/%.o: $(src_dir)/%.c $(c_headers)
+$(int_dir)/%.c.o: $(src_dir)/%.c $(c_headers)
 	@ mkdir -p $(dir $@)
 	$(PE_GCC) -ffreestanding $(compile_defs) $(efi_include_flags) -c $< -o $@
-$(int_dir)/%.o: $(src_dir)/%.cpp $(c_headers)
+$(int_dir)/%.cpp.o: $(src_dir)/%.cpp $(c_headers)
 	@ mkdir -p $(dir $@)
 	$(PE_GCC) -ffreestanding $(compile_defs) $(efi_include_flags) -c $< -o $@
