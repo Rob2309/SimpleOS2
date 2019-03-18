391a7da4dbd0eecf325b094d437afcc2d8199647
Robin Quint <rob2309@hotmail.de>
2019-03-18 07:36:18 +0100

    Fixed file names

--- makeinclude/elfso.mk
+++ makeinclude/elfso.mk
@@ -10,21 +10,21 @@ c_sources := $(wildcard $(src_dir)/*.c)
 cpp_sources := $(wildcard $(src_dir)/*.cpp)
 asm_sources := $(wildcard $(src_dir)/*.asm)
 
-c_objects := $(subst src/,int/$(config)/, $(subst .c,.o, $(c_sources)))
-cpp_objects := $(subst src/,int/$(config)/, $(subst .cpp,.o, $(cpp_sources)))
-asm_objects := $(subst src/,int/$(config)/, $(subst .asm,.o, $(asm_sources)))
+c_objects := $(subst src/,int/$(config)/, $(subst .c,.c.o, $(c_sources)))
+cpp_objects := $(subst src/,int/$(config)/, $(subst .cpp,.cpp.o, $(cpp_sources)))
+asm_objects := $(subst src/,int/$(config)/, $(subst .asm,.asm.o, $(asm_sources)))
 
 $(output_file): $(c_objects) $(cpp_objects) $(asm_objects)
 	@ mkdir -p $(dir $@)
 	$(ELF_GCC) -g -fPIC -nostdlib -shared -o $@ $^ -e main -static -lgcc
 
 
-$(int_dir)/%.o: $(src_dir)/%.c $(c_headers) $(cross_headers)
+$(int_dir)/%.c.o: $(src_dir)/%.c $(c_headers) $(cross_headers)
 	@ mkdir -p $(dir $@)
 	$(ELF_GCC) -g -fPIC -ffreestanding -fno-stack-protector -fno-exceptions $(compile_defs) $(elf_include_flags) -c $< -o $@
-$(int_dir)/%.o: $(src_dir)/%.cpp $(c_headers) $(cross_headers)
+$(int_dir)/%.cpp.o: $(src_dir)/%.cpp $(c_headers) $(cross_headers)
 	@ mkdir -p $(dir $@)
 	$(ELF_GCC) -g -fPIC -ffreestanding -fno-stack-protector -fno-exceptions $(compile_defs) $(elf_include_flags) -c $< -o $@
-$(int_dir)/%.o: $(src_dir)/%.asm $(c_headers) $(cross_headers)
+$(int_dir)/%.asm.o: $(src_dir)/%.asm $(c_headers) $(cross_headers)
 	@ mkdir -p $(dir $@)
 	$(NASM) -g -f elf64 $(compile_defs) $(elf_nasm_include_flags) $< -o $@
