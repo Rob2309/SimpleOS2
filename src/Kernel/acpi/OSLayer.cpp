872e5d95c5a8c7e0917956f4d8a7fdaa803f9436
Robin Quint <rob2309@hotmail.de>
2019-08-16 15:00:49 +0200

    WIP, hopefully improved config file

--- src/Kernel/acpi/OSLayer.cpp
+++ src/Kernel/acpi/OSLayer.cpp
@@ -1,16 +1,17 @@
 #include "types.h"
 
+extern "C" {
+#include "acpica/acpi.h"
+}
+
 #include "ACPI.h"
 #include "memory/MemoryManager.h"
 #include "scheduler/Scheduler.h"
 #include "klib/stdio.h"
 #include "time/Time.h"
-#include "locks/StickyLock.h"
+#include "atomic/Atomics.h"
 #include "locks/QueueLock.h"
-
-extern "C" {
-#include "acpica/acpi.h"
-}
+#include "locks/StickyLock.h"
 
 extern "C" {
 
@@ -21,17 +22,31 @@ extern "C" {
         return (ACPI_PHYSICAL_ADDRESS)MemoryManager::KernelToPhysPtr(ACPI::g_RSDP);
     }
 
+    ACPI_STATUS AcpiOsPredefinedOverride(const ACPI_PREDEFINED_NAMES* predefObj, ACPI_STRING* newVal) {
+        *newVal = nullptr;
+        return AE_OK;
+    }
+
     ACPI_STATUS AcpiOsTableOverride(ACPI_TABLE_HEADER* oldTable, ACPI_TABLE_HEADER** newTable) {
         *newTable = nullptr;
+        return AE_OK;
+    }
+
+    ACPI_STATUS AcpiOsPhysicalTableOverride(ACPI_TABLE_HEADER* oldTable, ACPI_PHYSICAL_ADDRESS* newAddr, UINT32* newTableLength) {
+        *newAddr = 0;
+        *newTableLength = 0;
         return AE_OK;
     }
 
     void* AcpiOsMapMemory(ACPI_PHYSICAL_ADDRESS phys, ACPI_SIZE size) {
         return MemoryManager::PhysToKernelPtr((void*)phys);
     }
-    void AcpiOsUnmapMemory(void* block, ACPI_SIZE size) { }
+    void AcpiOsUnmapMemory(void* virt, ACPI_SIZE size) { }
 
     ACPI_STATUS AcpiOsGetPhysicalAddress(void* virt, ACPI_PHYSICAL_ADDRESS* phys) {
+        if(virt == nullptr || phys == nullptr)
+            return AE_BAD_PARAMETER;
+
         *phys = (ACPI_PHYSICAL_ADDRESS)MemoryManager::KernelToPhysPtr(virt);
         return AE_OK;
     }
@@ -50,13 +65,22 @@ extern "C" {
         return true;
     }
 
-    ACPI_THREAD_ID AcpiOsGetThreadID() {
+    ACPI_THREAD_ID AcpiOsGetThreadId() {
         return Scheduler::ThreadGetTID();
     }
 
+    static Atomic<uint64> g_NumACPICAThreads;
+
+    static void AcpiThread(void (*func)(void*), void* arg) {
+        func(arg);
+        g_NumACPICAThreads.Dec();
+        Scheduler::ThreadExit(0);
+    }
+
     ACPI_STATUS AcpiOsExecute(ACPI_EXECUTE_TYPE type, ACPI_OSD_EXEC_CALLBACK func, void* arg) {
+        g_NumACPICAThreads.Inc();
         uint64 tid = Scheduler::CreateKernelThread((uint64)func, (uint64)arg);
-        klog_info("ACPICA", "Created ACPICA KernelThread (tid=%i)", tid);
+        klog_info("ACPICA", "Created kernel thread with tid %i", tid);
         return AE_OK;
     }
 
@@ -64,71 +88,61 @@ extern "C" {
         Scheduler::ThreadWait(millis);
     }
     void AcpiOsStall(UINT32 micros) {
+        Scheduler::ThreadSetSticky();
+
+        uint32 ticks = Time::GetTSCTicksPerMilli() / 1000 * micros;
+
         uint64 start = Time::GetTSC();
-
-        uint64 ticksPerMill = Time::GetTSCTicksPerMilli();
-
-        uint64 ticks = ticksPerMill / 1000 * micros;
-        
         while(true) {
             uint64 now = Time::GetTSC();
-            if(now - start > ticks)
+            if(now - start >= ticks)
                 break;
         }
+
+        Scheduler::ThreadUnsetSticky();
     }
 
-    /*ACPI_STATUS AcpiOsCreateMutex(ACPI_MUTEX* outHandle) {
-        *outHandle = new StickyLock();
+    void AcpiOsWaitEventsComplete() {
+        while(g_NumACPICAThreads.Read() > 0)
+            Scheduler::ThreadYield();
     }
-    void AcpiOsDeleteMutex(ACPI_MUTEX handle) {
-        delete (StickyLock*)handle;
-    }
-    ACPI_STATUS AcpiOsAcquireMutex(ACPI_MUTEX handle, UINT16 to) {
-        auto lock = (StickyLock*)handle;
-
-        if(to == 0) {
-            if(lock->TryLock())
-                return AE_OK;
-        } else if(to == 0xFFFF) {
-            lock->Spinlock_Raw();
-            return AE_OK;
-        } else {
-            uint64 start = Time::GetTSC();
-            while(true) {
-                uint64 now = Time::GetTSC();
-                if(now - start > Time::GetTSCTicksPerMilli() * to)
-                    return AE_TIME;
-
-                if(lock->TryLock())
-                    return AE_OK;
-            }
-        }
-
-        return AE_TIME;
-    }
-    void AcpiOsReleaseMutex(ACPI_MUTEX handle) {
-        ((StickyLock*)handle)->Unlock_Raw();
-    }*/
 
     ACPI_STATUS AcpiOsCreateSemaphore(UINT32 max, UINT32 init, ACPI_SEMAPHORE* outHandle) {
+        if(outHandle == nullptr)
+            return AE_BAD_PARAMETER;
+        
         *outHandle = new QueueLock(init);
         return AE_OK;
     }
     ACPI_STATUS AcpiOsDeleteSemaphore(ACPI_SEMAPHORE handle) {
+        if(handle == nullptr)
+            return AE_BAD_PARAMETER;
+
         delete (QueueLock*)handle;
+        return AE_OK;
     }
     ACPI_STATUS AcpiOsWaitSemaphore(ACPI_SEMAPHORE handle, UINT32 units, UINT16 to) {
-        auto q = (QueueLock*)handle;
+        if(handle == nullptr)
+            return AE_BAD_PARAMETER;
+
+        auto lock = (QueueLock*)handle;
         for(int i = 0; i < units; i++)
-            q->Lock();
+            lock->Lock();
+        return AE_OK;
     }
     ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE handle, UINT32 units) {
-        auto q = (QueueLock*)handle;
+        if(handle == nullptr)
+            return AE_BAD_PARAMETER;
+        auto lock = (QueueLock*)handle;
         for(int i = 0; i < units; i++)
-            q->Unlock();
+            lock->Unlock();
+        return AE_OK;
     }
 
     ACPI_STATUS AcpiOsCreateLock(ACPI_SPINLOCK* outHandle) {
+        if(outHandle == nullptr)
+            return AE_BAD_PARAMETER;
+
         *outHandle = new StickyLock();
         return AE_OK;
     }
@@ -136,22 +150,15 @@ extern "C" {
         delete (StickyLock*)handle;
     }
     ACPI_CPU_FLAGS AcpiOsAcquireLock(ACPI_SPINLOCK handle) {
-        auto lock = (StickyLock*)handle;
-        lock->Spinlock_Cli();
+        ((StickyLock*)handle)->Spinlock_Cli();
         return 0;
     }
-    void AcpiOsReleaseLock(ACPI_SPINLOCK handle, ACPI_CPU_FLAGS flags)  {
-        auto lock = (StickyLock*)handle;
-        lock->Unlock_Cli();
+    void AcpiOsReleaseLock(ACPI_SPINLOCK handle, ACPI_CPU_FLAGS flags) {
+        ((StickyLock*)handle)->Unlock_Cli();
     }
 
-    ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 intLevel, ACPI_OSD_HANDLER handler, void* Context) {
-        // TODO: Implement
-        return AE_NOT_IMPLEMENTED;
-    }
-    ACPI_STATUS AcpiOsRemoveInterruptHandler(UINT32 intNumber, ACPI_OSD_HANDLER handler) {
-        // TODO: implement
-        return AE_NOT_IMPLEMENTED;
+    ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 no, ACPI_OSD_HANDLER handle, void* arg) {
+        
     }
 
 }
--- src/acpica/include/acpica/platform/acsimpleos2.h
+++ src/acpica/include/acpica/platform/acsimpleos2.h
@@ -1,83 +1,19 @@
 #pragma once
 
-/* Common (in-kernel/user-space) ACPICA configuration */
-
-//#define ACPI_USE_SYSTEM_CLIBRARY
-#define ACPI_USE_DO_WHILE_0
-//#define ACPI_IGNORE_PACKAGE_RESOLUTION_ERRORS
-
-//#define ACPI_USE_SYSTEM_INTTYPES
-//#define ACPI_USE_GPE_POLLING
-
-#define ACPI_INIT_FUNCTION 
-
+#define ACPI_SPINLOCK void*
+#define ACPI_SEMAPHORE void*
 #define ACPI_MUTEX_TYPE ACPI_BINARY_SEMAPHORE
 
+#define ACPI_CPU_FLAGS UINT64
+
 #define ACPI_CACHE_T ACPI_MEMORY_LIST
-#define ACPI_USE_LOCAL_CACHE 1
+#define ACPI_USE_LOCAL_CACHE
 
-/* Host-dependent types and defines for in-kernel ACPICA */
+#define ACPI_USE_DO_WHILE_0
 
-#define ACPI_MACHINE_WIDTH          64
-#define ACPI_USE_NATIVE_MATH64
-#define ACPI_EXPORT_SYMBOL(symbol)  ;
-#define strtoul                     simple_strtoul
-
-#define ACPI_CACHE_T                int
-#define ACPI_SPINLOCK               void*
-#define ACPI_CPU_FLAGS              unsigned long
+#define ACPI_MACHINE_WIDTH 64
 
 #define va_list __builtin_va_list
-#define va_start __builtin_va_start
 #define va_arg __builtin_va_arg
+#define va_start __builtin_va_start
 #define va_end __builtin_va_end
-
-/* Use native linux version of AcpiOsAllocateZeroed */
-
-#define USE_NATIVE_ALLOCATE_ZEROED
-
-/*
- * Overrides for in-kernel ACPICA
- */
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsInitialize
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsTerminate
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsAllocate
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsAllocateZeroed
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsFree
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsAcquireObject
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetThreadId
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsCreateLock
-
-/*
- * OSL interfaces used by debugger/disassembler
- */
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsReadable
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsWritable
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsInitializeDebugger
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsTerminateDebugger
-
-/*
- * OSL interfaces used by utilities
- */
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsRedirectOutput
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetTableByName
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetTableByIndex
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetTableByAddress
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsOpenDirectory
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsGetNextFilename
-#define ACPI_USE_ALTERNATE_PROTOTYPE_AcpiOsCloseDirectory
-
-#undef ACPI_USE_LOCAL_CACHE
-
-#define ACPI_MSG_ERROR          "ACPI Error: "
-#define ACPI_MSG_EXCEPTION      "ACPI Exception: "
-#define ACPI_MSG_WARNING        "ACPI Warning: "
-#define ACPI_MSG_INFO           "ACPI: "
-
-#define ACPI_MSG_BIOS_ERROR     "ACPI BIOS Error (bug): "
-#define ACPI_MSG_BIOS_WARNING   "ACPI BIOS Warning (bug): "
-
-/*
- * Linux wants to use designated initializers for function pointer structs.
- */
-#define ACPI_STRUCT_INIT(field, value)  .field = value
